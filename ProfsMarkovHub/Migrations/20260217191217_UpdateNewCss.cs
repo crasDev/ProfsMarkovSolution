5e86e74197fe04415d27b822a9e84b12f6d7a3d1
ProfsDk <cras.dev@gmail.com>
2026-02-17 20:13:08 +0100

    save data

---
+++ ProfsMarkovHub/Migrations/20260217191217_UpdateNewCss.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProfsMarkovHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateNewCss : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Excerpt",
+                table: "Articles",
+                type: "TEXT",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Excerpt",
+                table: "Articles");
+        }
+    }
+}
